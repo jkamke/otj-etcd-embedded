1c07d7ba2871e4aaa2aeda14f6a3281422ad63b8
Steven Schlansker <sschlansker@opentable.com>
2014-04-01 10:56:34 -0700

    Dramatically improve API

--- embedded/src/main/java/com/opentable/etcd/EtcdConfiguration.java
+++ embedded/src/main/java/com/opentable/etcd/EtcdConfiguration.java
@@ -6,10 +6,16 @@ public class EtcdConfiguration
 {
     private Path dataDirectory;
     private boolean destroyNodeOnExit;
+    private String discoveryUri;
+    private String hostname;
+    private int clientPort;
+    private int peerPort;
+    private boolean verbose;
 
-    public void setDataDirectory(Path dataDirectory)
+    public EtcdConfiguration setDataDirectory(Path dataDirectory)
     {
         this.dataDirectory = dataDirectory;
+        return this;
     }
 
     public Path getDataDirectory()
@@ -17,13 +23,65 @@ public class EtcdConfiguration
         return dataDirectory;
     }
 
-    public void setDestroyNodeOnExit(boolean destroyNodeOnExit)
+    public EtcdConfiguration setDestroyNodeOnExit(boolean destroyNodeOnExit)
     {
         this.destroyNodeOnExit = destroyNodeOnExit;
+        return this;
     }
 
     public boolean isDestroyNodeOnExit()
     {
         return destroyNodeOnExit;
+    }
+
+    public void setDiscoveryUri(String discoveryUri)
+    {
+        this.discoveryUri = discoveryUri;
+    }
+
+    public String getDiscoveryUri()
+    {
+        return discoveryUri;
+    }
+
+    public void setHostname(String hostname)
+    {
+        this.hostname = hostname;
+    }
+
+    public String getHostname()
+    {
+        return hostname;
+    }
+
+    public void setClientPort(int clientPort)
+    {
+        this.clientPort = clientPort;
+    }
+
+    public int getClientPort()
+    {
+        return clientPort;
+    }
+
+    public void setPeerPort(int peerPort)
+    {
+        this.peerPort = peerPort;
+    }
+
+    public int getPeerPort()
+    {
+        return peerPort;
+    }
+
+    public EtcdConfiguration setVerbose(boolean verbose)
+    {
+        this.verbose = verbose;
+        return this;
+    }
+
+    public boolean isVerbose()
+    {
+        return verbose;
     }
 }
--- embedded/src/main/java/com/opentable/etcd/EtcdInstance.java
+++ embedded/src/main/java/com/opentable/etcd/EtcdInstance.java
@@ -3,11 +3,16 @@ package com.opentable.etcd;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.PosixFilePermissions;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +26,7 @@ import com.opentable.io.DeleteRecursively;
 
 public class EtcdInstance implements Closeable
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(EtcdServerRule.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EtcdInstance.class);
     private static final String ETCD_PACKAGE_PATH_FMT = "/etcd-binary-%s/etcd";
     private static final String ETCD_LOCATION;
 
@@ -32,7 +37,7 @@ public class EtcdInstance implements Closeable
         Path etcdFile = null;
         try {
             etcdFile = Files.createTempFile("etcd", "bin");
-            try (InputStream etcd = EtcdServerRule.class.getResourceAsStream(etcdPath)) {
+            try (InputStream etcd = EtcdInstance.class.getResourceAsStream(etcdPath)) {
                 if (etcd == null) {
                     throw new IllegalStateException("Could not find " + etcdPath + " on classpath");
                 }
@@ -70,25 +75,48 @@ public class EtcdInstance implements Closeable
     @GuardedBy("this")
     private int peerPort;
 
+
+    public void startUnchecked()
+    {
+        try {
+            start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public synchronized void start() throws IOException
     {
         if (etcdServer != null) {
             throw new IllegalStateException("already started");
         }
 
-        clientPort = findPort();
-        peerPort = findPort();
+        clientPort = findPort(configuration.getClientPort());
+        peerPort = findPort(configuration.getPeerPort());
 
-        final String clientAddr = "0.0.0.0:" + clientPort;
-        final String peerAddr = "0.0.0.0:" + peerPort;
-        etcdServer = new ProcessBuilder(ETCD_LOCATION,
-                "-v",
+        final String hostname = configuration.getHostname() != null ? configuration.getHostname() : findHostname();
+
+        final String clientAddr = hostname + ':' + clientPort;
+        final String peerAddr = hostname + ':' + peerPort;
+
+        final List<String> arguments = new ArrayList<>();
+        arguments.addAll(Arrays.asList(
+                ETCD_LOCATION,
                 "-data-dir", configuration.getDataDirectory().toString(),
                 "-name", id,
                 "-addr", clientAddr,
-                "-bind-addr", clientAddr,
-                "-peer-addr", peerAddr,
-                "-peer-bind-addr", peerAddr).inheritIO().start();
+                "-peer-addr", peerAddr));
+
+        if (configuration.isVerbose()) {
+            arguments.add("-v");
+        }
+
+        if (configuration.getDiscoveryUri() != null) {
+            arguments.add("-discovery");
+            arguments.add(configuration.getDiscoveryUri());
+        }
+
+        etcdServer = new ProcessBuilder(arguments).inheritIO().start();
 
         try {
             Thread.sleep(200);
@@ -143,10 +171,18 @@ public class EtcdInstance implements Closeable
         return etcdServer != null;
     }
 
-    private static int findPort() throws IOException
+    private static int findPort(int configuredPort) throws IOException
     {
+        if (configuredPort != 0) {
+            return configuredPort;
+        }
         try (ServerSocket socket = new ServerSocket(0)) {
             return socket.getLocalPort();
         }
+    }
+
+    private static String findHostname() throws UnknownHostException
+    {
+        return InetAddress.getLocalHost().getHostName();
     }
 }
